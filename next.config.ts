8c9716afd8a3c2f2ea89a18241234f6e14923182
Lokinder Singh Chauhan <lokinder.chauhan@gmail.com>
2024-11-08 17:50:10 +0530

    added .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
   basePath: "/os-dev-pages",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
